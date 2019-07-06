593e2bcd9dba508558277d7a3c21f1cccc91ec92
haythemlp <haythamov1993@gmail.com>
2019-07-06 17:22:04 +0100

    fix

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -12,16 +12,13 @@ export class AuthGuardService implements CanActivate {
     constructor(private router: Router, private auth: AuthentificationService) {
     }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> |
-        Promise<boolean | UrlTree> | boolean | UrlTree {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
         console.log('guard!');
 
         if (!localStorage.getItem('user')) {
             this.router.navigate(['/auth/login']);
             return false;
         } else {
-
-
             let storage = JSON.parse(localStorage.getItem('user'));
             return this.auth.me(storage.access_token).pipe(map(response => {
                     console.log(response);
@@ -33,8 +30,6 @@ export class AuthGuardService implements CanActivate {
                         console.log(error);
                         localStorage.clear();
                         this.router.navigate(['auth/login']);
-
-
                         return Observable.throw(error);
                     }
                 )
